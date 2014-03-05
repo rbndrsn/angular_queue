9f3a36b9044107142216c49a80b748ce3a580102
Rob Anderson <rbndrsn1980@gmail.com>
2014-03-05 17:41:31 +0000

    sort add and remove from guest list

--- app/app/scripts/controllers/user.coffee
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-angular.module('queueApp')
-  .controller 'UserCtrl', ($scope) ->
-    $scope.guests = [
-      {name:'Rob'},
-      {name:'Danielle'}
-    ]
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,30 @@
 'use strict';
 
 angular.module('queueApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('guestCtrl', function ($scope) {
     $scope.guests = [
-      {name:'Rob'},
-      {name:'Danielle'}
+      {name:'Rob', done:true},
+      {name:'Danielle', done:false}
     ];
-  });
+
+    $scope.addGuest = function() {
+      $scope.guests.push({name:$scope.guestName, done:false});
+      $scope.guestName = '';
+    };
+
+    $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.guests, function(guest) {
+      count += guest.done ? 0 : 1;
+    });
+    return count;
+    };
+
+    $scope.archive = function() {
+    var oldGuests = $scope.guests;
+    $scope.guests = [];
+    angular.forEach(oldGuests, function(guest) {
+      if (!guest.done) $scope.guests.push(guest);
+    });
+  };
+});
